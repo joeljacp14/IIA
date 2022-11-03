b34a7b2bdc10bbb8407248c5d1d440e0a62f6497
Joel Jacome Pioquinto <joeljacomep@outlook.com>
2022-11-03 14:17:31 -0600

    meta

--- Practicas_Python/AjedrezTest.py
+++ Practicas_Python/AjedrezTest.py
@@ -11,13 +11,15 @@ estado_inicial = [
                     ["bT", "bC", "bA", "bQ", "bK", "bA", "bC", "bT"]
                 ]
 
-meta = [
-    [],
-    [],
-    [],
-    [],
-    [],
-    []
+meta = [# jaque mate del loco (se pierde un peon negro, donde esta la reina blanca) GANAN LAS FICHAS BLANCAS
+                    ["nT", "__", "nA", "nQ", "nK", "nA", "__", "nT"],
+                    ["nP", "nP", "nP", "nP", "__", "bQ", "nP", "nP"],
+                    ["__", "__", "nC", "__", "__", "nC", "__", "__"],
+                    ["__", "__", "__", "__", "nP", "__", "__", "__"],
+                    ["__", "__", "bA", "__", "bP", "__", "__", "__"],
+                    ["__", "__", "__", "__", "__", "__", "__", "__"],
+                    ["bP", "bP", "bP", "bP", "__", "bP", "bP", "bP"],
+                    ["bT", "bC", "bA", "__", "bK", "__", "bC", "bT"]
 ]
 
 raiz = ajedrez.Nodo(estado_inicial)
