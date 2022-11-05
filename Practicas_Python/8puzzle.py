132ef9b51d1367fe7dbcc6caab5611c975f42efc
Joel Jacome Pioquinto <joeljacomep@outlook.com>
2022-11-04 23:50:25 -0600

    heuristica 8-puzzle

--- Practicas_Python/8puzzle.py
+++ Practicas_Python/8puzzle.py
@@ -1,5 +1,6 @@
 from mimetypes import init
 import copy as copiar
+import math as mate
 
 class Nodo(object):
     def __init__(self, estado):
@@ -7,9 +8,9 @@ class Nodo(object):
         self.estado = estado
         self.hijos = []
     
-    def expande(self):
-        ren, col = self.busca_espacio()
-        if (not ren and not col):
+    def expande(self, ficha):
+        ren, col = self.busca_ficha(ficha)
+        if (not ren or not col):
             return None
         
         #generar arriba
@@ -36,13 +37,13 @@ class Nodo(object):
             izquierda[ren][col - 1] = self.estado[ren][col]
             izquierda[ren][col] = self.estado[ren][col - 1]
             self.hijos.append(Nodo(izquierda))
-
-    def busca_espacio(self):
+    
+    def busca_ficha(self, ficha):
         col = 0
         reg = 0
         for renglon in self.estado:
             for columna in renglon:
-                if columna == "_":
+                if columna == ficha:
                     return(reg, col)
                 col = col + 1
             reg = reg + 1
@@ -55,9 +56,34 @@ class Nodo(object):
                 print(columna, end = " ")
             print(" ")
         print(" ")
+    
+    def heuristica(self, meta):
+        sum = 0
+        re = ce = rm = cm = 0
+        for i in range(8):
+            rm, cm = meta.busca_ficha(i + 1)
+            re, ce = self.busca_ficha(i + 1)
+            sum += mate.sqrt(mate.pow((rm - re), 2)) + mate.sqrt(mate.pow((cm - ce), 2))
+        rm, cm = meta.busca_ficha("_")
+        re, ce = self.busca_ficha("_")
+        sum += mate.sqrt(mate.pow((rm - re), 2)) + mate.sqrt(mate.pow((cm - ce), 2))
+        return sum
 
+estado_inicial = [
+    [7, 1, 6],
+    [4, "_", 8],
+    [3, 5, 2]
+]
 
-raiz = Nodo([["_", 1, 6], [4, 3, 8], [7, 5, 2]])
+meta = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, "_"]
+]
+
+raiz = Nodo(estado_inicial)
+nodoMeta = Nodo(meta)
+print("Heuristica", raiz.heuristica(nodoMeta))
 print("Estado inicial")
 raiz.imprime_estado()
 raiz.expande()
