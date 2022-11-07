9c59942c384efc5f4e12d11cf3284578e6b2a577
Joel Jacome Pioquinto <joeljacomep@outlook.com>
2022-11-07 15:00:38 -0600

    busquedas ajedrez

--- Practicas_Python/Ajedrez.py
+++ Practicas_Python/Ajedrez.py
@@ -1,11 +1,50 @@
-
+import string
 class Nodo(object):
     def __init__(self, estado):
         super(Nodo, self).__init__()
         self.estado = estado
+        self.heur = 0
         self.hijos = []
     
-    def expande(self):
+    def expande_peon(self, color):
+        if (color == 'b'):
+            pass
+        else:
+            pass
+
+    def expande_alfil(self):
+        pass
+
+    def expande_caballo(self):
+        pass
+
+    def expande_reina(self):
+        pass
+
+    def busca_peon_del_rey(self, color):
+        col = reg = 0
+        for r in self.estado:
+            for c in r:
+                if color == 'n':
+                    if c == "nP" and self.estado[reg - 1, col] == "nK":
+                        return (reg, col)
+                else:
+                    if c == "bP" and self.estado[reg + 1, col] == "bK":
+                        return (reg, col)
+                col += 1
+            reg += 1
+        return None, None
+
+    def busca_alfil(self):
+        col = reg = 0
+        for r in self.estado:
+            for c in r:
+                if c == "bA":
+                    if self.estado[reg][col - 1] == "bK" and self.estado[reg][col + 1] == "bC":
+                        return (reg, col)
+        return (None, None)
+
+    def busca_caballo(self):
         pass
 
     def imprime_estado(self):
