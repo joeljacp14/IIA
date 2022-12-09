3541c3aa00fa67737803e7be9ac1fa8960705a0c
Joel Jacome Pioquinto <joeljacomep@outlook.com>
2022-12-09 17:45:31 -0600

    EL FANTASMA NO PUEDE SEGUIR EL CAMINO AIUDAAAAAAAAAAAAAA

--- Godot_Projects/IA2022/RedGhost.gd
+++ Godot_Projects/IA2022/RedGhost.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 
 var pos : Vector2
-var nodo : Vector2
+var nodo_map : Vector2
+var nodo_world : Vector2
 var speed = 50
 var way = []
 onready var player = get_parent().get_node("PacMan")
@@ -24,23 +25,31 @@ onready var tile_map = get_parent().get_node("Obstaculos")
 func _process(delta):
 	pos.x = 0
 	pos.y = 0
-	move_and_slide(pos)
-	print(tile_map.map_to_world(Vector2(1, 1)))
+	#move_and_slide(pos)
+	var curren_position = global_position
+	print("Current position world: ", curren_position)
+	curren_position = tile_map.world_to_map(Vector2(int(round(curren_position.x)), int(round(curren_position.y))))
+	#curren_position = Vector2(int(round(curren_position.x)), int(round(curren_position.y)))
+	
 	if way == []:
-		print("Se termino el camino")
 		return
-	nodo = way.pop_front()
-	print("Nodo map ", nodo)
-	nodo = tile_map.map_to_world(nodo)
-	print("Nodo world", nodo)
-	print("Current position ", global_position)
-	if global_position.y == nodo.y:
-		pos.x = nodo.x * delta * speed
-		pos.y = 0
-	if global_position.x == nodo.x:
-		pos.x = 0
-		pos.y = nodo.y * delta * speed
 		
+	nodo_map = way.pop_front()
+	nodo_world = tile_map.map_to_world(nodo_map)
+	nodo_world.x = nodo_world.x + 32
+	nodo_world.y = nodo_world.y + 32
+	
+	print("Nodo map ", nodo_map)
+	print("Nodo world centered", nodo_world)
+	print("Current position map: ", curren_position)
+	
+	if curren_position.y == nodo_map.y:
+		pos.x = nodo_world.x
+		pos.y = 0
+	if curren_position.x == nodo_map.x:
+		pos.x = 0
+		pos.y = nodo_world.y
+	
 	print("Moverse a ", pos)
 	print("====================================")
 	
