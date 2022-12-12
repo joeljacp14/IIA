b07bb86b1b883276db71262b39754a9485ec9444
leissacs <leissamariana1234@gmail.com>
2022-12-12 15:08:30 -0600

    PROYECTO MAMALON

--- Godot_Projects/PacMan Tuneado/fantasm_N.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-onready var walls = get_find_parent().get_node("walls")
-
-func _ready():
-	position = walls.get_enemy_pos()
-	pass
-# Declare member variables here. Examples:
-# var a = 2
